d28b04590857c604d941c40fc40f881447322467
chidraeve <lukas.mainka@clubitsolutions.de>
2020-12-30 21:50:32 +0100

    32. Moving Repos State to Context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useContext, Fragment } from "react"
-import PropTypes from "prop-types"
 import Spinner from "../layout/spinner"
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -7,16 +6,16 @@ import { faCheck, faTimesCircle } from "@fortawesome/free-solid-svg-icons"
 import Repos from "../repos/Repos"
 import GithubContext from "../../context/github/githubContext"
 
-const User = ({ getUserRepos, repos, match }) => {
+const User = ({ match }) => {
 	const githubContext = useContext(GithubContext)
+
+	const { getUser, loading, user, repos, getUserRepos } = githubContext
 
 	useEffect(() => {
 		getUser(match.params.login)
 		getUserRepos(match.params.login)
 		// eslint-disable-next-line
 	}, [])
-
-	const { getUser, loading, user } = githubContext
 
 	const {
 		name,
@@ -102,11 +101,6 @@ const User = ({ getUserRepos, repos, match }) => {
 			<Repos repos={repos} />
 		</Fragment>
 	)
-}
-
-User.propTypes = {
-	repos: PropTypes.array.isRequired,
-	getUserRepos: PropTypes.func.isRequired
 }
 
 export default User
