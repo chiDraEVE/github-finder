bb26d917de1baf32f1112d223980d915f3434db6
chidraeve <lukas.mainka@clubitsolutions.de>
2020-12-30 23:43:27 +0100

    34. Home & NotFound Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React, { Fragment } from "react"
+import React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import "./App.css"
 import Navbar from "./components/layout/Navbar"
-import Users from "./components/users/Users"
-import Search from "./components/users/Search"
 import Alert from "./components/layout/Alert"
 import About from "./components/pages/About"
 import User from "./components/users/User"
@@ -13,6 +11,8 @@ import { fas } from "@fortawesome/free-solid-svg-icons"
 
 import GithubState from "./context/github/GithubState"
 import AlertState from "./context/alert/AlertState"
+import Home from "./components/pages/Home"
+import NotFound from "./components/pages/NotFound"
 
 library.add(fab, fas)
 
@@ -28,18 +28,10 @@ const App = () => {
 						<div className='container'>
 							<Alert />
 							<Switch>
-								<Route
-									exact
-									path='/'
-									render={() => (
-										<Fragment>
-											<Search />
-											<Users />
-										</Fragment>
-									)}
-								/>
+								<Route exact path='/' component={Home} />
 								<Route exact path='/about' component={About} />
 								<Route exact path='/user/:login' component={User} />
+								<Route component={NotFound} />
 							</Switch>
 						</div>
 					</div>
