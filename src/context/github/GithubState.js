418fffbdfced8e89a3a7093286cc1bac86ae1c8e
chidraeve <lukas.mainka@clubitsolutions.de>
2020-12-30 23:51:21 +0100

    35. Prepare for Deployment I am not gonna use netlify though but deploy it on my Dedicated Linux Server later

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -18,14 +18,25 @@ const GithubState = (props) => {
 		loading: false
 	}
 
+	let githubClientId
+	let githubClientSecret
+
+	if (process.env.NODE_ENV !== "production") {
+		githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID
+		githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET
+	} else {
+		githubClientId = process.env.GITHUB_CLIENT_ID
+		githubClientSecret = process.env.GITHUB_CLIENT_SECRET
+	}
+
 	const [state, dispatch] = useReducer(GithubReducer, initialState)
 
 	// Search Github users
 	const searchUsers = async (text) => {
 		setLoading(true)
 		const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
-        ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-        ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+        ${githubClientId}&client_secret=
+        ${githubClientSecret}`)
 		dispatch({ type: SEARCH_USERS, payload: res.data.items })
 	}
 
@@ -33,8 +44,8 @@ const GithubState = (props) => {
 	const getUser = async (username) => {
 		setLoading(true)
 		const res = await axios.get(`https://api.github.com/users/${username}?client_id=
-        ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-        ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+        ${githubClientId}&client_secret=
+        ${githubClientSecret}`)
 
 		dispatch({
 			type: GET_USER,
@@ -45,8 +56,8 @@ const GithubState = (props) => {
 	const getUserRepos = async (username) => {
 		setLoading(true)
 		const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
-        ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-        ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+        ${githubClientId}&client_secret=
+        ${githubClientSecret}`)
 		dispatch({ type: GET_REPOS, payload: res.data })
 	}
 
